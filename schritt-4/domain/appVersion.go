be79542595c7d5d774b4cdd1f98d655ce4cc0abc
Dr. Gernot Starke <gs@gernotstarke.de>
2022-05-03 10:30:08 +0200

    get the date right

--- schritt-4/domain/appVersion.go
+++ schritt-4/domain/appVersion.go
@@ -4,4 +4,4 @@ import "time"
 
 const VersionStr = "0.0.4"
 
-var AppName = "PDFminion - pre-alpha " + time.Now().Format("01-Jan-2006")
+var AppName = "PDFminion - pre-alpha " + time.Now().Format("2. Jan 2006")
