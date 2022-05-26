1110c1b73d418dc48bb94e6174900673ba2130cd
Dr. Gernot Starke <gs@gernotstarke.de>
2022-05-26 16:00:38 +0200

    Revert "Merge branch 'step-2' of https://github.com/gernotstarke/step-by-step-architecture into step-1"
    This reverts commit 6fbda231c80dd756f30455550bc7078f481f6323, reversing
    changes made to 0c69b6c7b689c0b6ff3e7f2d63c6ae3ff6a9ab2e.

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module schritt-2
+module schritt-1
 
 go 1.18
 
--- main.go
+++ main.go
@@ -1,54 +1,39 @@
-// prepare for automated testing
 package main
 
 import (
-	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
 )
 
-type protoui struct {
-	app    fyne.App
-	win    fyne.Window
-	status *widget.Label
-	input  *widget.Entry
-	accept *widget.Button
-	cancel *widget.Button
-}
-
-func makeUI(p *protoui) fyne.CanvasObject {
-
-	// application and visible window
-	p.app = app.New()
-	p.win = p.app.NewWindow("Take-2")
-
-	// widgets
-	p.status = widget.NewLabel("")
-	p.input = widget.NewEntry()
-	p.input.PlaceHolder = "enter something"
-
-	// upon button press, set status widget
-	p.accept = widget.NewButton("Action", func() {
-		p.status.SetText(p.input.Text)
-	})
-
-	// cancel button quits application
-	p.cancel = widget.NewButton("Cancel", func() {
-		p.app.Quit()
-	})
-
-	return container.NewVBox(p.status, p.input,
-		container.New(layout.NewHBoxLayout(), p.cancel, p.accept))
-}
-
 func main() {
 
-	// initialize prototype UI
-	p := &protoui{}
-	content := makeUI(p)
+	// Appl exposes the fyne application - mainly to enable the quit-function.
+	appl := app.New()
 
-	p.win.SetContent(content)
-	p.win.ShowAndRun()
+	// Window is the main application window
+	window := appl.NewWindow("Schritt-1")
+
+	entryField := widget.NewEntry()
+	entryField.SetText("")
+
+	statusLabel := widget.NewLabel("")
+
+	actionButton := widget.NewButton("Action", func() {
+		statusLabel.SetText(entryField.Text)
+	})
+
+	cancelButton := widget.NewButton("Cancel", func() {
+		appl.Quit()
+	})
+
+	content := container.New(layout.NewVBoxLayout(),
+		entryField,
+		statusLabel,
+		container.New(layout.NewHBoxLayout(), cancelButton, actionButton))
+
+	window.SetContent(content)
+	window.ShowAndRun()
+
 }
