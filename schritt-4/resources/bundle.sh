3250d5b730c7e5a3105165a0a41985225811a7f1
Dr. Gernot Starke <gs@gernotstarke.de>
2022-05-26 15:40:51 +0200

    keep only step-2

--- schritt-4/resources/bundle.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-fyne bundle -name Arc42LogoPNG -package resources arc42-logo.png  > bundled.go
-fyne bundle -name PDFminionLogoPNG -append PDFminion-logo.png  >> bundled.go
-# splash image still missing
